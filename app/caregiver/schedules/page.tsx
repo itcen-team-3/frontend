da384157adf542a88d861b3dcf6748da01326c2c
ciocio97 <tmddusleo@icloud.com>
2025-06-02 16:51:45 +0900

    feat: add caregiver schedules page #11 Closes #11

---
+++ app/caregiver/schedules/page.tsx
@@ -0,0 +1,5 @@
+import { CaregiverScheduleCalendar } from "@/components/schedules/caregiver-schedule-calendar";
+
+export default function ScheduleCalendarPage() {
+  return <CaregiverScheduleCalendar caregiverName={""} scheduleEvents={[]} />;
+}
