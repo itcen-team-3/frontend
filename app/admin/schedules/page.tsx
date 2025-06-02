7d3dde5d7fef19e92f4224c73559a7242885b435
ciocio97 <tmddusleo@icloud.com>
2025-06-02 17:17:16 +0900

    feat: add admin schedules page #15 Closes #15

---
+++ app/admin/schedules/page.tsx
@@ -0,0 +1,7 @@
+import { AdminScheduleOverview } from "@/components/schedules/admin-schedule-overview";
+
+export default function ScheduleOverviewPage() {
+  return (
+    <AdminScheduleOverview caregivers={[]} patients={[]} scheduleEvents={[]} />
+  );
+}
