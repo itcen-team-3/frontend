be8579dcf18c951a5b89d3a027ed0ca17ff7cdc2
ciocio97 <tmddusleo@icloud.com>
2025-06-02 17:42:52 +0900

    feat: add admin patients detail page #20 Closes #20

---
+++ app/admin/patients/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { PatientDetail } from "@/components/patients/patient-detail";
+
+export default function PatientDetailPage() {
+  return (
+    <PatientDetail
+      id={""}
+      name={""}
+      age={0}
+      address={""}
+      phone={""}
+      medicalNotes={""}
+      familyContact={""}
+      schedules={[]}
+    />
+  );
+}
