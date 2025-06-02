77950aac0fff386827902f007c60daa4db8e52be
ciocio97 <tmddusleo@icloud.com>
2025-06-02 17:11:43 +0900

    feat: add admin caregivers new page #25 Closes #25

---
+++ app/admin/caregivers/new/page.tsx
@@ -0,0 +1,5 @@
+import { CaregiverRegistrationForm } from "@/components/caregivers/caregiver-registration-form";
+
+export default function NewCaregiverPage() {
+  return <CaregiverRegistrationForm />;
+}
