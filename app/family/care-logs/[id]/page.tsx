9c4f5511daad8020bb911931c41deec2d1ce60fa
ciocio97 <tmddusleo@icloud.com>
2025-06-03 17:35:28 +0900

    fix: parameter build error #66 Closes #66

--- app/family/care-logs/[id]/page.tsx
+++ app/family/care-logs/[id]/page.tsx
@@ -1,10 +1,8 @@
+import { PageProps } from "@/.next/types/app/page";
 import { FamilyCareLogDetail } from "@/components/care-logs/family-care-log-detail";
 
-export default function FamilyCareLogDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function FamilyCareLogDetailPage({ params }: PageProps) {
+  const { id } = await params;
   // 실제 구현에서는 params.id를 사용하여 API에서 돌봄일지 데이터를 가져옵니다
-  return <FamilyCareLogDetail id={params.id} />;
+  return <FamilyCareLogDetail id={id} />;
 }
