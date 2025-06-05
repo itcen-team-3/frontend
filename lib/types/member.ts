40497b427746bbfefd494a9b9b61e08e8992d93d
ciocio97 <tmddusleo@icloud.com>
2025-06-05 17:43:47 +0900

    feat: add sign-up type #100

--- lib/types/member.ts
+++ lib/types/member.ts
@@ -14,4 +14,20 @@ interface SignInResponse {
   };
 }
 
-export { type SignInRequest, type SignInResponse };
+// sign-up
+interface SignUpRequest {
+  loginId: string;
+  loginPw: string;
+  loginPwConfirm: string;
+  representativeName: string;
+  birthDate: string; // LocalDate → ISO string (e.g., '2024-05-05')
+  companyName: string;
+  companyAddress: string;
+  email: string;
+  businessRegistrationFile: File; // MultipartFile → File 타입 사용
+  businessRegistrationNumber: string;
+  openingDate: string;
+  phoneNumber: string;
+}
+
+export { type SignInRequest, type SignInResponse, type SignUpRequest };
