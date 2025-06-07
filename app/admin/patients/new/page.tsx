48c8c794eae782a474810468dac5df53ea76a2d7
ciocio97 <tmddusleo@icloud.com>
2025-06-07 17:00:45 +0900

    feat: add admin create patient api #86 Closes #86

--- app/admin/patients/new/page.tsx
+++ app/admin/patients/new/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
 import { PatientRegistrationForm } from "@/components/patients/patient-registration-form";
+import { useCreatePatient } from "@/features/member/useCreatePatient";
 
 export default function NewPatientPage() {
-  return <PatientRegistrationForm />;
+  const { createPatient, isLoading, error } = useCreatePatient();
+
+  return (
+    <PatientRegistrationForm
+      isLoading={isLoading}
+      error={error}
+      onClickCreatePatientButton={(value) => createPatient(value)}
+    />
+  );
 }
