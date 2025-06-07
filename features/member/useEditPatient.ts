340fd307c1eb1aecbadc62ed8cb0b78f2e85e99e
ciocio97 <tmddusleo@icloud.com>
2025-06-07 18:38:42 +0900

    feat: add admin patient info edit api #85 Closes #85

---
+++ features/member/useEditPatient.ts
@@ -0,0 +1,65 @@
+"use client";
+
+import { useState } from "react";
+import { api } from "@/lib/http";
+import { useRouter } from "next/navigation";
+import type { PatientInfoRequest } from "@/lib/types/member";
+
+export const useEditPatient = () => {
+  const router = useRouter();
+
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    code: 0,
+    message: "",
+  });
+
+  const editPatient = async (
+    patientId: string | undefined,
+    data: PatientInfoRequest
+  ) => {
+    if (!patientId) return;
+
+    setLoading(true);
+    setError({ code: 0, message: "" });
+
+    try {
+      const formData = new FormData();
+      formData.append("name", data.name);
+      // TODO 생일 변환 로직 빼고 테스트해보기
+      formData.append(
+        "birthDate",
+        data.birthDate?.toISOString()?.split("T")[0] || ""
+      );
+      formData.append("phoneNumber", data.phoneNumber);
+      formData.append("address", data.address);
+      formData.append("patientLevel", data.patientLevel);
+      formData.append("guardianPhoneNumber", data.guardianPhoneNumber);
+      formData.append("guardianName", data.guardianName);
+      formData.append("relationship", data.relationship);
+      formData.append("description", data.description);
+
+      if (data.profileImageFile) {
+        formData.append("profileImage", data.profileImageFile);
+      }
+
+      const res = await api.put_form(`/member/patient/${patientId}`, {
+        body: formData,
+      });
+
+      if (res.code < 300) {
+        console.log("보호대상자 수정 성공");
+        router.push("/admin/patients");
+      }
+    } catch (e: any) {
+      setError({
+        code: e?.code || 0,
+        message: e?.message || "알 수 없는 오류가 발생했습니다.",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { editPatient, isLoading, error };
+};
