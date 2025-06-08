23423d66552fcecd5358e0348d2fd8264e4dfdbc
ciocio97 <tmddusleo@icloud.com>
2025-06-08 11:49:01 +0900

    feat: add getStartofWeek util funtion

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getStartOfWeek(date = new Date()) {
+  const day = date.getDay(); // 0: 일요일, 1: 월요일, ..., 6: 토요일
+  const diff = day === 0 ? -6 : 1 - day;
+
+  const monday = new Date(date);
+  monday.setDate(date.getDate() + diff);
+
+  // YYYY-MM-DD 포맷으로 반환
+  const year = monday.getFullYear();
+  const month = String(monday.getMonth() + 1).padStart(2, "0");
+  const dayOfMonth = String(monday.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${dayOfMonth}`;
+}
