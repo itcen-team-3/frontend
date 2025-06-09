5f892195f148546fb53bc6dee20644f67a8c309b
ciocio97 <tmddusleo@icloud.com>
2025-06-09 17:33:11 +0900

    feat: add console

--- app/admin/accounts/page.tsx
+++ app/admin/accounts/page.tsx
@@ -1,7 +1,5 @@
 import { AccountManagement } from "@/components/accounts/account-management";
 
 export default function AccountsPage() {
-  console.log("test");
-  console.log("wontae");
   return <AccountManagement accounts={[]} />;
 }
