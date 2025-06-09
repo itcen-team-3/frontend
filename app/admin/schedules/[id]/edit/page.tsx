28e0c16c9d064e622db0ab5a49436c32cb5649bd
ciocio97 <tmddusleo@icloud.com>
2025-06-09 17:13:11 +0900

    feat: add caregiver and patient name list #89

--- app/admin/schedules/[id]/edit/page.tsx
+++ app/admin/schedules/[id]/edit/page.tsx
@@ -2,10 +2,21 @@
 
 import { ScheduleForm } from "@/components/schedules/schedule-form";
 import { WorkScheduleRequest } from "@/lib/types/workSchedule";
+import { useGetCaregiverNameList } from "@/features/member/useGetCaregiverNameList";
+import { useGetPatientNameList } from "@/features/member/useGetPatientNameList";
+import Loading from "@/components/ui/loading-page";
 
 export default function EditSchedulePage() {
+  const {
+    caregiverNameList,
+    isCaregiverNameListLoading,
+    errorCaregiverNameList,
+  } = useGetCaregiverNameList();
+  const { patientNameList, isPatientNameListLoading, errorPatientNameList } =
+    useGetPatientNameList();
   // 실제 구현에서는 ID를 기반으로 데이터를 가져옵니다
   // TODO : 추후 제거 build 에러 해결용
+  // TODO : api 연동
   const mockData = {
     caregiverId: null,
     patientName: "",
@@ -21,12 +32,19 @@ export default function EditSchedulePage() {
     isFamily: false,
   };
 
+  // TODO : 이 로직 검토
+  if (isCaregiverNameListLoading || isPatientNameListLoading) {
+    console.log("errorCaregiverNameList", errorCaregiverNameList);
+    console.log("errorPatientNameList", errorPatientNameList);
+    return <Loading />;
+  }
+
   return (
     <ScheduleForm
       mode="edit"
       initialData={mockData}
-      caregiverNameList={[]}
-      patientNameList={[]}
+      caregiverNameList={caregiverNameList?.caregivers || []}
+      patientNameList={patientNameList?.patients || []}
       isLoading={false}
       error={{
         code: 0,
