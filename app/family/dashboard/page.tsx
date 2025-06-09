30efb3fc8687301e3416bd010109fb2646f52f49
yunikhyeon <goodthth@naver.com>
2025-06-09 17:22:47 +0900

    feat: add console

--- app/family/dashboard/page.tsx
+++ app/family/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import { FamilyDashboard } from "@/components/dashboard/family-dashboard";
 
 export default function FamilyDashboardPage() {
+  console.log("Family Dashboard Page");
   return (
     <FamilyDashboard
       familyName={""}
