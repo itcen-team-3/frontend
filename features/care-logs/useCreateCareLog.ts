7a4a3a960f3af2e03c7ac1d1cb067843a856f9ca
ciocio97 <tmddusleo@icloud.com>
2025-06-12 10:48:11 +0900

    feat: add caregiver create care-log #75 #69 Closes #75 Closes #69

--- features/care-logs/useCreateCareLog.ts
+++ features/care-logs/useCreateCareLog.ts
@@ -18,10 +18,48 @@ export const useCreateCareLog = () => {
     setError({ code: 0, message: "" });
 
     try {
-      console.log("data", data);
       const formData = new FormData();
 
-      const res = await api.post_form("/carelog", {
+      formData.append("patientId", data.patientId);
+
+      const keys = Object.keys(data.durations);
+      for (let i = 0; i < keys.length; i++) {
+        formData.append(`careItemList[${i}].careItemId`, keys[i]);
+        formData.append(
+          `careItemList[${i}].requiredMinutes`,
+          data.durations[`${keys[i]}`]
+        );
+      }
+
+      data.photos.forEach((photo: any, index: number) => {
+        if (photo.file) {
+          formData.append(
+            `imageDtoList[${index}].imageType`,
+            photo.type === "식사" ? "MEAL" : "MEDICATION"
+          );
+          formData.append(`imageDtoList[${index}].imageFile`, photo.file);
+        }
+      });
+
+      function dataURLtoBlob(dataurl: any) {
+        const arr = dataurl.split(",");
+        const mime = arr[0].match(/:(.*?);/)[1];
+        const bstr = atob(arr[1]);
+        let n = bstr.length;
+        const u8arr = new Uint8Array(n);
+        while (n--) {
+          u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new Blob([u8arr], { type: mime });
+      }
+
+      const blob = dataURLtoBlob(data.signFile);
+      const file = new File([blob], "signature.png", { type: "image/png" });
+
+      formData.append("signFile", file);
+      formData.append("description", data.description);
+
+      const res = await api.post_form("/care_log", {
         body: formData,
       });
 
