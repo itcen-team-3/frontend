c11df61039859bc8b4ea255712b79ff5a34394e8
ciocio97 <tmddusleo@icloud.com>
2025-06-12 19:33:16 +0900

    feat: add caregiver schedule api #76 Closes #76

--- app/caregiver/schedules/page.tsx
+++ app/caregiver/schedules/page.tsx
@@ -1,5 +1,33 @@
+"use client";
+
+import { useState } from "react";
 import { CaregiverScheduleCalendar } from "@/components/schedules/caregiver-schedule-calendar";
+import { useGetCaregiverWorkSchedulesByDay } from "@/features/workschedule/useGetCaregiverWorkSchedulesByDay";
+import { useGetCaregiverWorkSchedulesByWeek } from "@/features/workschedule/useGetCaregiverWorkSchedulesByWeek";
+import { startOfWeek } from "date-fns";
 
 export default function ScheduleCalendarPage() {
-  return <CaregiverScheduleCalendar caregiverName={""} scheduleEvents={[]} />;
+  const [date, setDate] = useState(new Date());
+  const [startDateOfWeek, setDtartOfWeek] = useState(
+    startOfWeek(new Date(), { weekStartsOn: 1 })
+  );
+
+  const { caregiverScheduleByDay, isCaregiverScheduleByDayLoading } =
+    useGetCaregiverWorkSchedulesByDay(date);
+  const { caregiverScheduleByWeek, isCaregiverScheduleByWeekLoading } =
+    useGetCaregiverWorkSchedulesByWeek(startDateOfWeek);
+
+  console.log("caregiverScheduleByWeek", caregiverScheduleByWeek);
+
+  return (
+    <CaregiverScheduleCalendar
+      date={date}
+      setDate={setDate}
+      caregiverScheduleByDay={caregiverScheduleByDay?.scheduleDay || []}
+      isCaregiverScheduleByDayLoading={isCaregiverScheduleByDayLoading}
+      setDtartOfWeek={setDtartOfWeek}
+      caregiverScheduleByWeek={caregiverScheduleByWeek?.scheduleWeek || []}
+      isCaregiverScheduleByWeekLoading={isCaregiverScheduleByWeekLoading}
+    />
+  );
 }
